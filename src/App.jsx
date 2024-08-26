b450cd9b59ddbd5be0113c9700216a3477679aa9
ItaloKaua1 <italo.kaua@alu.ufc.br>
2024-08-26 10:00:10 -0300

    set project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
       <div>
-        
+        <Home/>
       </div>
     </>
   )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css'
+
+const Home = () => {
+    return(
+        <div className='containerDefault' id='divHome'>
+            <h1>PRIMEIRA TELA</h1>
+        </div>
+    )
+}
+
+export default Home
