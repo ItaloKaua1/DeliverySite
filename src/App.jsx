9dd95b2928eb1de1c7e731ad12cc0341c0a08634
ItaloKaua1 <italo.kaua@alu.ufc.br>
2024-08-29 09:56:36 -0300

    create Apresentation Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css' 
+import ApresentationScreen from './pages/ApresentationScreen/ApresentationScreen'
 import Home from './pages/Home/Home'
 
 function App() {
@@ -6,7 +7,8 @@ function App() {
   return (
     <>
       <div className='divApp'>
-        <Home />
+        {/* <Home /> */}
+        <ApresentationScreen />
       </div>
     </>
   )
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -6,33 +6,33 @@ function Menu (){
             {/* Marmita */}
             <section className='divCardapio' id="#divMontar">
                 <p id='titulo'>Marmita</p>
-                <div className='divMarmita'>
-                    <p className='marmita'>Monte sua marmita</p>
+                <div className='divOpcao'>
+                    <p className='nome'>Monte sua marmita</p>
                     <p id="preco">R$ 15,00</p>
-                    <img src="/Marmita_post.jpg" alt="Marmita" id="imgMarmita"/>
+                    <img src="/Marmita_post.jpg" alt="Marmita" className="imgMenu"/>
                 </div>
             </section>
 
             {/* Combos */}
             <section className='divCardapio' id="divCombos">
                 <p id='titulo'>Combos</p>
-                <div className='divMarmita'>
-                    <p className='marmita'>Marmita + Refrigerante Lata</p>
+                <div className='divOpcao'>
+                    <p className='nome'>Marmita + Refrigerante Lata</p>
                     <p id="preco">R$ 18,50</p>
-                    <img src="/Marmita_post.jpg" alt="Marmita" id="imgMarmita"/>
+                    <img src="/Marmita_post.jpg" alt="Marmita" className="imgMenu"/>
                 </div>
-                <div className='divMarmita' id="divCombo2">
-                    <p className='marmita'>Marmita + Refrigerante Lata</p>
+                <div className='divOpcao' id="space">
+                    <p className='nome'>2 Marmitas + Cocada Caseira + Refri 1L</p>
                     <p id="preco">R$ 18,50</p>
-                    <img src="/Marmita_post.jpg" alt="Marmita" id="imgMarmita"/>
+                    <img src="/Marmita_post.jpg" alt="Marmita" className="imgMenu"/>
                 </div>
             </section>
 
             {/* Sobremas */}
             <section className='divCardapio' id="divSobremesas">
                 <p id='titulo'>Sobremesas</p>
-                <div className='divMarmita'>
-                    <p className='marmita'>Cocada caseira</p>
+                <div className='divOpcao'>
+                    <p className='nome'>Cocada caseira</p>
                     <p id="preco">R$ 4,50</p>
                     <img src="/cocada_post.jpeg" alt="Marmita" id="imgCocada"/>
                 </div>
@@ -41,25 +41,25 @@ function Menu (){
             {/* Bebidas */}
             <section className='divCardapio' id="divBebidas">
                 <p id='titulo'>Bebidas</p>
-                <div className='divMarmita'>
-                    <p className='marmita'>Refrigerante 1L</p>
+                <div className='divOpcao'>
+                    <p className='nome'>Refrigerante 1L</p>
                     <p id="preco">R$ 7,00</p>
-                    <img src="/Refrigerante_1l_post.jpeg" alt="Marmita" id="imgMarmita"/>
+                    <img src="/Refrigerante_1l_post.jpeg" alt="Refri1L" className="imgMenu"/>
                 </div>
-                <div className='divMarmita' id="divCombo2">
-                    <p className='marmita'>Refrigerante 350ml</p>
+                <div className='divOpcao' id="space">
+                    <p className='nome'>Refrigerante 350ml</p>
                     <p id="preco">R$ 3,50</p>
-                    <img src="/Refrigerante_mini_post.jpeg" alt="Marmita" id="imgMarmita"/>
+                    <img src="/Refrigerante_mini_post.jpeg" alt="Refri350ml" className="imgMenu"/>
                 </div>
-                <div className='divMarmita' id="divCombo2">
-                    <p className='marmita'>Refrigerante Lata</p>
+                <div className='divOpcao' id="space">
+                    <p className='nome'>Refrigerante Lata</p>
                     <p id="preco">R$ 4,50</p>
-                    <img src="/post_refri_lata" alt="Marmita" id="imgMarmita"/>
+                    <img src="/post_refri_lata" alt="RefriLata" className="imgMenu"/>
                 </div>
-                <div className='divMarmita' id="divCombo2">
-                    <p className='marmita'>Suco 300ml</p>
+                <div className='divOpcao' id="space">
+                    <p className='nome'>Suco 300ml</p>
                     <p id="preco2">R$ 18,50</p>
-                    <img src="/Suco_post.jpeg" alt="Marmita" id="imgSuco"/>
+                    <img src="/Suco_post.jpeg" alt="Suco" id="imgSuco"/>
                 </div>
             </section>
         </>
