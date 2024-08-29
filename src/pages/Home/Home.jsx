01a8f46315c98a1a23ae16526aebc745c2417f33
ItaloKaua1 <italo.kaua@alu.ufc.br>
2024-08-28 21:24:27 -0300

    ajuste div principal home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import HorarioFuncionamento from '../../components/HorarioFuncionamento/HorarioFuncionamento'
+import Logo from '../../components/Logo/Logo'
 import Menu from '../../components/Menu/Menu'
 import Navbar from '../../components/Navbar/Navbar'
 import SearchBar from '../../components/SearchBar/SearchBar'
@@ -10,6 +11,7 @@ const items = ["Marmita", "Combos", "Sobremesas", "Bebidas"]
 const Home = () => {
     return(
         <div className='containerDefault' id='divHome'>
+            <Logo />
             <Navbar />
             <HorarioFuncionamento />
             <SearchBar items={items}/>
