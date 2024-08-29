13b46b3b2102002307490b65d9ea73fad2149a12
ItaloKaua1 <italo.kaua@alu.ufc.br>
2024-08-29 00:25:58 -0300

    flexbox cardapio opcao

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,14 +3,14 @@ import "./Menu.css"
 function Menu (){
     return(
         <>
-            <div className='divCardapio'>
+            <section className='divCardapio'>
                 <p id='titulo'>Marmita</p>
                 <div id='divMarmita'>
                     <p className='marmita'>Monte sua marmita</p>
-                    <p id='preco'>R$ 15,00</p>
+                    <p id="preco">R$ 15,00</p>
                     <img src="/Marmita_post.jpg" alt="Marmita" id="imgMarmita"/>
                 </div>
-            </div>
+            </section>
         </>
     )
 }
