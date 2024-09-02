cf8af125453beec34ff1f12a80f80f34e68f6868
Vitor <vitorloula3@gmail.com>
2024-09-02 12:38:14 -0300

    Melhora na aparência da NavBar

--- Front/src/components/Navbar/Navbar.jsx
+++ Front/src/components/Navbar/Navbar.jsx
@@ -1,29 +1,37 @@
-import { Link } from 'react-router-dom';
-import './Navbar.css'
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <div id="NavbarDiv" >
-            <div className='containerItems' id='cardapio'>
-                <img src="/Cardapio_vector.svg" alt="cardapio" className='img'/>
-                <p id='textoNav'><strong>Cardápio</strong></p>
-            </div>
-            <div className='containerItems' id='sacola'>
-                <img src="/Sacola.svg" alt="sacola" className='img'/>
-                <p id='textoNav'><strong>Sacola</strong></p>
-            </div>
-            <div className='containerItems' id='pedidos'>
-                <img src="/Pedidos.svg" alt="pedidos" className='img'/>
-                <p id='textoNav'><strong>Pedidos</strong></p>
-            </div>
-            <Link to="/Perfil" className="Link">
-                <div className='containerItems' id='perfil'>
-                    <img src="/perfil.svg" alt="perfil" className='img'/>
-                    <p id='textoNav'><strong>Perfil</strong></p>
-                </div>
-            </Link>
+  return (
+    <div id="NavbarDiv">
+      <div className="containerItems" id="cardapio">
+        <img src="/Cardapio_vector.svg" alt="Cardápio" className="img" />
+        <p id="textoNav">
+          <strong>Cardápio</strong>
+        </p>
+      </div>
+      <div className="containerItems" id="sacola">
+        <img src="/Sacola.svg" alt="Sacola" className="img" />
+        <p id="textoNav">
+          <strong>Sacola</strong>
+        </p>
+      </div>
+      <div className="containerItems" id="pedidos">
+        <img src="/Pedidos.svg" alt="Pedidos" className="img" />
+        <p id="textoNav">
+          <strong>Pedidos</strong>
+        </p>
+      </div>
+      <Link to="/Perfil" className="Link">
+        <div className="containerItems" id="perfil">
+          <img src="/perfil.svg" alt="Perfil" className="img" />
+          <p id="textoNav">
+            <strong>Perfil</strong>
+          </p>
         </div>
-    )
-}
+      </Link>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
