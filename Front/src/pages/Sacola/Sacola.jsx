41865897d9116633c33d304a482f1e5caae042f6
ItaloKaua1 <italo.kaua@alu.ufc.br>
2024-09-04 11:58:50 -0300

    navbar icon preenchimento onclick

---
+++ Front/src/pages/Sacola/Sacola.jsx
@@ -0,0 +1,14 @@
+import Logo from "../../components/Logo/Logo"
+
+const Sacola = () => {
+    return(
+        <div className="containerDefault">
+            <Logo />
+
+            <h1>Resumo do pedido:</h1>
+
+        </div>
+    )
+}
+
+export default Sacola
