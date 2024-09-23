8d0b7b3baa99ef43200028cac41fca19f50eb8ff
Pedro Henrique Santos Moreira <moreira.phs@alu.ufc.br>
2024-09-23 20:46:50 -0300

    e agora??

--- Front/src/components/Navbar/Navbar.jsx
+++ Front/src/components/Navbar/Navbar.jsx
@@ -3,60 +3,80 @@ import "./Navbar.css";
 import { useState, useEffect } from "react";
 
 const Navbar = () => {
-  const [activeIcon, setActiveIcon] = useState("cardapio"); // Estado único para controlar o ícone ativo
+  const location = useLocation();
 
-  const handleIconClick = (iconName) => {
-    setActiveIcon(iconName); // Atualiza o ícone ativo
-  };
+  const [cardapioCheio, setCardapioCheio] = useState(false);
+  const [sacolaCheia, setSacolaCheia] = useState(false);
+  const [pedidosCheio, setPedidosCheio] = useState(false);
+  const [perfilCheio, setPerfilCheio] = useState(false);
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/Home":
+        setCardapioCheio(true);
+        setSacolaCheia(false);
+        setPedidosCheio(false);
+        setPerfilCheio(false);
+        break;
+      case "/Sacola":
+        setCardapioCheio(false);
+        setSacolaCheia(true);
+        setPedidosCheio(false);
+        setPerfilCheio(false);
+        break;
+      case "/Pedidos":
+        setCardapioCheio(false);
+        setSacolaCheia(false);
+        setPedidosCheio(true);
+        setPerfilCheio(false);
+        break;
+      case "/Perfil":
+        setCardapioCheio(false);
+        setSacolaCheia(false);
+        setPedidosCheio(false);
+        setPerfilCheio(true);
+        break;
+      default:
+        setCardapioCheio(true);
+        setSacolaCheia(false);
+        setPedidosCheio(false);
+        setPerfilCheio(false);
+        break;
+    }
+  }, [location.pathname]);
 
   return (
     <div id="NavbarDiv">
-      <Link to="/Home" className="Link" onClick={() => handleIconClick("cardapio")}>
+      <Link to="/Home" className="Link">
         <div className="containerItems" id="cardapio">
-          <img
-            src={activeIcon === "cardapio" ? '/Cardapio_vector.svg' : '/cardapioVazio.png'}
-            alt="Cardápio"
-            className="img"
-          />
+          <img src={cardapioCheio ? '/Cardapio_vector.svg' : '/cardapioVazio.png'} alt="Cardápio" className="img" />
           <p id="textoNav">
             <strong>Cardápio</strong>
           </p>
         </div>
       </Link>
 
-      <Link to="/Sacola" className="Link" onClick={() => handleIconClick("sacola")}>
+      <Link to="/Sacola" className="Link">
         <div className="containerItems" id="sacola">
-          <img
-            src={activeIcon === "sacola" ? '/sacola-preenchida.svg' : '/Sacola.svg'}
-            alt="Sacola"
-            className="img"
-          />
+          <img src={sacolaCheia ? '/sacola-preenchida.svg' : '/Sacola.svg'} alt="Sacola" className="img" />
           <p id="textoNav">
             <strong>Sacola</strong>
           </p>
         </div>
       </Link>
 
-      <Link to="/Pedidos" className="Link" onClick={() => handleIconClick("pedidos")}>
+      <Link to="/Pedidos" className="Link">
         <div className="containerItems" id="pedidos">
-          <img
-            src={activeIcon === "pedidos" ? '/pedidosCheio.svg' : '/Pedidos.svg'}
-            alt="Pedidos"
-            className="img"
-          />
+          <img src={pedidosCheio ? '/pedidosCheio.svg' : '/Pedidos.svg'} alt="Pedidos" className="img" />
           <p id="textoNav">
             <strong>Pedidos</strong>
           </p>
         </div>
       </Link>
 
-      <Link to="/Perfil" className="Link" onClick={() => handleIconClick("perfil")}>
+      <Link to="/Perfil" className="Link">
         <div className="containerItems" id="perfil">
-          <img
-            src={activeIcon === "perfil" ? '/perfilCheio.svg' : '/perfil.svg'}
-            alt="Perfil"
-            className="img"
-          />
+          <img src={perfilCheio ? '/perfilCheio.svg' : '/perfil.svg'} alt="Perfil" className="img" />
           <p id="textoNav">
             <strong>Perfil</strong>
           </p>
