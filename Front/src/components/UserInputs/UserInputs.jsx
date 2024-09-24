1111d35d188581d8f39dc942b93165fee3851040
ItaloKaua1 <italo.kaua@alu.ufc.br>
2024-09-24 13:37:49 -0300

    pefil finalizado

--- Front/src/components/UserInputs/UserInputs.jsx
+++ Front/src/components/UserInputs/UserInputs.jsx
@@ -1,16 +1,36 @@
 import "./UserInputs.css";
+import React, { useState } from "react";
 
 function UserInputs(props) {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+    };
+
     return (
         <div id="divUserI">
             <label id="nameU"><strong>Nome</strong></label>
-            <input id="nameUser" />
+            <input id="nameUser" placeholder="nome" />
 
             <label id="emailU"><strong>E-mail</strong></label>
-            <input type="email" id="emailUser"/>
+            <input type="email" id="emailUser" placeholder="exemplo@email.com" />
 
             <label id="passwordU"><strong>Senha</strong></label>
-            <input type="password" id="passwordUser"/>
+            <div className="password-container">
+                <input
+                    type={passwordVisible ? "text" : "password"}
+                    id="passwordUser"
+                    placeholder="●●●●●●●●"
+                />
+                <img
+                    src={passwordVisible ? "/olho_aberto.png" : "/visibility_off.png"}
+                    alt="Toggle visibility"
+                    className="toggle-password-icon"
+                    onClick={togglePasswordVisibility}
+                />
+            </div>
+
         </div>
     );
 }
